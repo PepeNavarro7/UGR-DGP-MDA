9870154281c2695a611cc3e63a398b614a979cc6
Jose Miguel <jmiguelmh3000@gmail.com>
2022-10-07 13:25:17 +0200

    Setup completado

---
+++ lib/Globales.dart
@@ -0,0 +1,11 @@
+library globales;
+
+import 'package:flutter/material.dart';
+
+const ColorBarraApp = Colors.green;
+const ColorFondo = Colors.white;
+const ColorBotones = Colors.green;
+const ColorTexto = Colors.white;
+
+const double AlturaBoton = 150;
+const double AnchuraBoton = 150;
