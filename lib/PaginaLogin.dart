09ef3f76e8c1a2c7d0289ba8983b9527e8eb4a0d
José Miguel Márquez Herreros <jmiguelmh3000@gmail.com>
2022-10-10 18:10:18 +0200

    Código mejor estructurado con funciones

--- lib/PaginaLogin.dart
+++ lib/PaginaLogin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 int NumeroPagina = 0;
 int NumeroFilas = 4;
@@ -19,49 +20,53 @@ class _PaginaLoginState extends State<PaginaLogin> {
     double TamanioLetra = EscalaTexto * 20;
     NumeroCasillasTotales = NumeroFilas * NumeroColumnas;
 
-    void RealizarAccion(String value) {
+    AlertDialog MenuAjustes() {
+      return AlertDialog(
+        content: Container(
+          height: 150,
+          child: Column(
+            children: [
+              Text('Tamaño iconos:', style: TextStyle(fontSize: TamanioLetra)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                      child: Text("2x1", style: TextStyle(fontSize: TamanioLetra, color: Colors.black)),
+                      onPressed: () {
+                        setState(() {
+                          NumeroPagina = 0;
+                          NumeroFilas = 2;
+                          NumeroColumnas = 1;
+                          NumeroCasillasTotales = NumeroFilas * NumeroColumnas;
+                        });
+                      }
+                  ),
+                  TextButton(
+                      child: Text("4x2", style: TextStyle(fontSize: TamanioLetra, color: Colors.black)),
+                      onPressed: () {
+                        setState(() {
+                          NumeroPagina = 0;
+                          NumeroFilas = 4;
+                          NumeroColumnas = 2;
+                          NumeroCasillasTotales = NumeroFilas * NumeroColumnas;
+                        });
+                      }
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    void RealizarAccionPopupMenu(String value) {
       switch (value) {
         case "Ajustes":
           showDialog(
               context: context,
               builder: (BuildContext context) {
-                return AlertDialog(
-                  content: Container(
-                    height: 150,
-                    child: Column(
-                      children: [
-                        Text('Tamaño iconos:', style: TextStyle(fontSize: TamanioLetra)),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            TextButton(
-                                child: Text("2x1", style: TextStyle(fontSize: TamanioLetra, color: Colors.black)),
-                                onPressed: () {
-                                  setState(() {
-                                    NumeroPagina = 0;
-                                    NumeroFilas = 2;
-                                    NumeroColumnas = 1;
-                                    NumeroCasillasTotales = NumeroFilas * NumeroColumnas;
-                                  });
-                                }
-                            ),
-                            TextButton(
-                                child: Text("4x2", style: TextStyle(fontSize: TamanioLetra, color: Colors.black)),
-                                onPressed: () {
-                                  setState(() {
-                                    NumeroPagina = 0;
-                                    NumeroFilas = 4;
-                                    NumeroColumnas = 2;
-                                    NumeroCasillasTotales = NumeroFilas * NumeroColumnas;
-                                  });
-                                }
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                );
+                return MenuAjustes();
               }
           );
           break;
@@ -82,87 +87,113 @@ class _PaginaLoginState extends State<PaginaLogin> {
       });
     }
 
+    Widget PopupMenu () {
+      return PopupMenuButton <String>(
+        onSelected: RealizarAccionPopupMenu,
+        itemBuilder: (BuildContext context) {
+          return {'Ajustes'}.map((String choice) {
+            return PopupMenuItem<String>(
+              value: choice,
+              child: Text(choice),
+            );
+          }).toList();
+        },
+      );
+    }
+
+    Widget Autenticacion() {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Página Login", style: TextStyle(fontSize: TamanioLetra)),
+          backgroundColor: Colors.green,
+          automaticallyImplyLeading: false,
+        ),
+      );
+    }
+
+    Widget IconosUsuarios() {
+      return Container(
+        height: MediaQuery.of(context).size.height * 0.75,
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.fromLTRB(15, 10, 15, 0),
+        child: GridView.builder(
+          itemCount: NumeroCasillasTotales,
+          physics: NeverScrollableScrollPhysics(),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: NumeroColumnas,
+              crossAxisSpacing: 4.0,
+              mainAxisSpacing: 4.0,
+              childAspectRatio: 1.2
+          ),
+          itemBuilder: (BuildContext context, int index){
+            index += NumeroPagina * NumeroCasillasTotales;
+            if(index >= Usuarios.length) {
+              return Container();
+            } else {
+              return Container(color: Colors.red,
+                  margin: EdgeInsets.all(2),
+                  child: Center(
+                    child: TextButton(
+                      child: Text(Usuarios[index], style: TextStyle(fontSize: TamanioLetra, color: Colors.black)),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => Autenticacion()));
+                      },
+                    ),
+                  )
+              );
+            }
+          },
+        ),
+      );
+    }
+
+    Widget Flechas() {
+      return Expanded(
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Visibility(
+                    visible: NumeroPagina > 0,
+                    child: IconButton(
+                      icon: new Icon(Icons.arrow_back, size: 70.0),
+                      onPressed: PaginaAnterior,
+                    ),
+                  )
+              ),
+              SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Visibility(
+                    visible: NumeroPagina < (Usuarios.length / NumeroCasillasTotales - 1),
+                    child: IconButton(
+                      icon: new Icon(Icons.arrow_forward, size: 70.0),
+                      onPressed: PaginaSiguiente,
+                    ),
+                  )
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
         title: Text("Página Login", style: TextStyle(fontSize: TamanioLetra)),
         actions: <Widget>[
-          PopupMenuButton <String>(
-            onSelected: RealizarAccion,
-            itemBuilder: (BuildContext context) {
-              return {'Ajustes'}.map((String choice) {
-                return PopupMenuItem<String>(
-                  value: choice,
-                  child: Text(choice),
-                );
-              }).toList();
-            },
-          ),
+          PopupMenu(),
         ],
       ),
       body: Column(
         children: <Widget>[
-          Container(
-            height: MediaQuery.of(context).size.height * 0.75,
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.fromLTRB(15, 10, 15, 0),
-            child: GridView.builder(
-              itemCount: NumeroCasillasTotales,
-              physics: NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: NumeroColumnas,
-                  crossAxisSpacing: 4.0,
-                  mainAxisSpacing: 4.0,
-                  childAspectRatio: 1.2
-              ),
-              itemBuilder: (BuildContext context, int index){
-                index += NumeroPagina * NumeroCasillasTotales;
-                if(index >= Usuarios.length) {
-                  return Container();
-                } else {
-                  return Container(color: Colors.red,
-                      margin: EdgeInsets.all(2),
-                      child: Center(
-                        child: Text(Usuarios[index],
-                            style: TextStyle(fontSize: TamanioLetra)),
-                      )
-                  );
-                }
-              },
-            ),
-          ),
-          Expanded(
-            child: Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(
-                      height: 100,
-                      width: 100,
-                      child: Visibility(
-                        visible: NumeroPagina > 0,
-                        child: IconButton(
-                          icon: new Icon(Icons.arrow_back, size: 70.0),
-                          onPressed: PaginaAnterior,
-                        ),
-                      )
-                  ),
-                  SizedBox(
-                      height: 100,
-                      width: 100,
-                      child: Visibility(
-                        visible: NumeroPagina < (Usuarios.length / NumeroCasillasTotales - 1),
-                        child: IconButton(
-                          icon: new Icon(Icons.arrow_forward, size: 70.0),
-                          onPressed: PaginaSiguiente,
-                        ),
-                      )
-                  ),
-                ],
-              ),
-            ),
-          ),
+          IconosUsuarios(),
+          Flechas(),
         ],
       ),
     );
