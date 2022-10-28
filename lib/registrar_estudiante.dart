7d5f8672b62dbc531d42c92191347d9d08b40f8a
José Miguel Márquez Herreros <jmiguelmh3000@gmail.com>
2022-10-28 11:31:12 +0200

    Avances en la interfaz de Registrar Estudiante

--- lib/registrar_estudiante.dart
+++ lib/registrar_estudiante.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
 
 /*
  * Clase Registrar Estudiante herede de StatefulWidget para que el campo de la
@@ -16,11 +16,6 @@ final List<String> tipoDeAccesos = <String>["Alfanumerico", "Pictogramas"];
 // Lista de tipo String que contiene todos los grados de accesibilidad
 final List<String> gradosDeAccesibilidad = <String>["Cognitiva", "Motora", "Visual"];
 
-// Lista con los pictogramas para el campo de la contraseña
-List<XFile> pictogramas = <XFile>[];
-
-// Lista de pictogramas para el campo de la contraseña
-List listaPictogramas = [];
 
 class RegistrarEstudiante extends StatefulWidget {
   @override
@@ -43,18 +38,133 @@ class _RegistrarEstudianteState extends State<RegistrarEstudiante> {
   // Valor del desplegable del grado de accesibilidad, por defecto es la primera opción
   String valorGradoAccesibilidad = gradosDeAccesibilidad.first;
 
-  File? image;
+  // Controlador para gestionar la selección de los pictogramas
+  final ImagePicker selectorImagenes = ImagePicker();
+
+  // Lista que almacena todas los pictogramas seleccionados por el usuario
+  List<XFile>? listaPictogramas = [];
+
+  // Función para seleccionar los pictogramas de la galería
+  void seleccionarPictogramas() async {
+    final List<XFile>? pictogramasSeleccionados = await selectorImagenes.pickMultiImage();
+
+    if (pictogramasSeleccionados!.isNotEmpty) {
+      listaPictogramas!.addAll(pictogramasSeleccionados);
+
+      if (listaPictogramas!.length > 4) {
+        listaPictogramas!.length = 4;
+      }
+    }
+
+    setState(() {});
+  }
+
+  // Función para vacir la lista de pictogramas
+  void eliminarPictogramas() {
+    setState(() {
+      listaPictogramas!.clear();
+    });
+  }
+
+  // Función para seleccionar la foto de perfil del estudiante de la galería o camara
+  void seleccionarFotoEstudiante() async {
+
+  }
+
+  Widget FotoEstudiante() {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        height: 250,
+        width: 250,
+        padding: EdgeInsets.fromLTRB(separacionElementos, separacionElementos, separacionElementos, 0.0),
+        child: CircleAvatar(
+          backgroundImage: AssetImage("assets/imagenes/sin_foto_perfil.jpg"),
+        ),
+      ),
+    );
+  }
+
+  // Widget que contiene la vista del campo contraseña, cambia según el tipo de acceso
+  Widget PasswordWidget(String tipoDeAcceso) {
+    if (tipoDeAcceso == "Alfanumerico") {
+      return PasswordAlfanumerico();
+    } else {
+      return PasswordPictogramas();
+    }
+  }
+
+  // Widget para la contraseña alfanumérica
+  Widget PasswordAlfanumerico() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(separacionElementos, separacionElementos, separacionElementos, 0.0),
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: "Contraseña",
+        ),
+      ),
+    );
+  }
+
+  // Widget para la contraseña por pictogramas
+  Widget PasswordPictogramas() {
+      return Column(
+        children: [
+          cuadriculaPictogramas(),
+          Container(
+            padding: EdgeInsets.fromLTRB(separacionElementos, separacionElementos, separacionElementos, 0.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: listaPictogramas!.length < 4 ? seleccionarPictogramas : null,
+                  child: Text("Añadir Pictogramas"),
+                  style: ElevatedButton.styleFrom(
+                    primary: colorElevatedButton,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: listaPictogramas!.isNotEmpty ? eliminarPictogramas : null,
+                  child: Text("Eliminar Pictogramas"),
+                  style: ElevatedButton.styleFrom(
+                    primary: colorElevatedButton,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      );
+  }
+
+  // Widget para mostrar los pictogramas seleccionados en una cuadricula de 2x2
+  Widget cuadriculaPictogramas() {
+    return SizedBox(
+      height: listaPictogramas!.length == 0 ? 0.0 : (listaPictogramas!.length < 3 ? MediaQuery.of(context).size.width / 2 : MediaQuery.of(context).size.width),
+      child: GridView.builder(
+        physics: NeverScrollableScrollPhysics(),
+        itemCount: listaPictogramas!.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return Image.file(File(listaPictogramas![index].path), fit: BoxFit.cover);
+        },
+      ),
+    );
+  }
 
   /*
    * Devuelve un Widget para mostrar el menú de Registrar Estudiante, con todos
    * sus elementos. Los elementos que lo conforman son:
-   *    1. Nombre (TextField)
-   *    2. Apellidos (TextField)
-   *    3. Email (TextField)
-   *    4. Tipo de Acceso (DropDown)
-   *    5. Grado de Accesibilidad (DropDown)
-   *    6. Contraseña (TextField o Pictogramas)
-   *    7. Añadir foto (ElevatedButton)
+   *    1. Añadir foto (Imagen como botón)
+   *    2. Nombre (TextField)
+   *    3. Apellidos (TextField)
+   *    4. Email (TextField)
+   *    5. Tipo de Acceso (DropDown)
+   *    6. Grado de Accesibilidad (DropDown)
+   *    7. Contraseña (TextField o Pictogramas)
    *    8. Registrar (ElevatedButton)
    */
   @override
@@ -67,6 +177,9 @@ class _RegistrarEstudianteState extends State<RegistrarEstudiante> {
       body: SafeArea(
         child: ListView(
           children: [
+            // Añadir foto
+            FotoEstudiante(),
+
             // Nombre
             Container(
               padding: EdgeInsets.fromLTRB(separacionElementos, separacionElementos, separacionElementos, 0.0),
@@ -142,24 +255,12 @@ class _RegistrarEstudianteState extends State<RegistrarEstudiante> {
 
             // Contraseña
             Container(
-              child: passwordWidget(valorTipoAcceso),
-            ),
-
-            // Añadir foto
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.fromLTRB(separacionElementos, separacionElementos, separacionElementos, 0.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text("Añadir Foto"),
-                style: ElevatedButton.styleFrom(
-                  primary: colorElevatedButton,
-                ),
-              ),
+              child: PasswordWidget(valorTipoAcceso),
             ),
 
             //Registrar
             Container(
+              alignment: Alignment.center,
               padding: EdgeInsets.fromLTRB(separacionElementos, separacionElementos, separacionElementos, separacionElementos),
               child: ElevatedButton(
                 onPressed: () {},
@@ -174,22 +275,6 @@ class _RegistrarEstudianteState extends State<RegistrarEstudiante> {
         ),
       ),
     );
-  }
-
-  Widget passwordWidget(String tipoDeAcceso) {
-    if (tipoDeAcceso == "Alfanumerico") {
-      return Container(
-        padding: EdgeInsets.fromLTRB(separacionElementos, separacionElementos, separacionElementos, 0.0),
-        child: TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            hintText: "Contraseña",
-          ),
-        ),
-      );
-    } else {
-      return Container();
-    }
   }
 }
 
