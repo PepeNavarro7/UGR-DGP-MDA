83c6572bdb636811ea3c6c6ae19d51856b2ba7f5
Jose Miguel <jmiguelmh3000@gmail.com>
2022-11-06 21:20:21 +0100

    Interfaz crear tarea

--- frontend/lib/vistas/crear_tarea.dart
+++ frontend/lib/vistas/crear_tarea.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 // Lista de tipo String que contiene todos los lugares de una tarea
-final List<String> listaLugares = <String>["Lugar 1", "Lugar 2", "Lugar 3"];
+final List<String> listaLugares = ["Lugar 1", "Lugar 2", "Lugar 3"];
 
 /*
  * Clase Crear Tarea hereda de StatefulWidget para que los distintos elementos
@@ -30,10 +30,29 @@ class _CrearTareaState extends State<CrearTarea> {
   String descripcion = "";
   String fecha = "";
   String lugar = listaLugares.first;
-  List<String> pasos = [];
+  String paso = "";
+  List<String> listaPasos = [];
 
   // Cotrolador del campo fecha
   TextEditingController controladorFecha = TextEditingController();
+
+  // Función que añade un paso a la lista de pasos, solo añade si no es null, el texto
+  // no son solo espacio y no esta repetido el paso
+  void aniadirPaso(String paso) {
+    setState(() {
+      if (paso != null && paso.trim().length != 0 && !listaPasos.contains(paso)) {
+        listaPasos.add(paso);
+        print(listaPasos.length);
+      }
+    });
+  }
+
+  // Función que borrar todos los pasos de una lista
+  void borrarPasos() {
+    setState(() {
+      listaPasos.clear();
+    });
+  }
 
   /*
    * Devuelve un Widget para mostrar el menú de Crear Tarea, con todos
@@ -136,36 +155,72 @@ class _CrearTareaState extends State<CrearTarea> {
                   },
                 )
             ),
-
-            // Pasos
+            
+            // TextField para escribir el paso a añadir
             Container(
-                width: MediaQuery.of(context).size.width - separacionElementos,
-                padding: EdgeInsets.fromLTRB(separacionElementos, separacionElementos, separacionElementos, 0.0),
+              padding: EdgeInsets.fromLTRB(separacionElementos, separacionElementos, separacionElementos, 0.0),
+              child: TextField(
+                onChanged: (text) {
+                  paso = text;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Paso",
+                ),
+              ),
             ),
 
-            // Botones Cancelar y Crear
+            // Botones para añadir y quitar pasos
             Container(
-              width: MediaQuery.of(context).size.width - separacionElementos,
               padding: EdgeInsets.fromLTRB(separacionElementos, separacionElementos, separacionElementos, 0.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   ElevatedButton(
-                    onPressed: () {},
-                    child: Text("Cancelar"),
+                    onPressed: borrarPasos,
                     style: ElevatedButton.styleFrom(
                       primary: Colors.red,
                     ),
+                    child: Text("Borrar pasos"),
                   ),
-
                   ElevatedButton(
-                    onPressed: () {},
-                    child: Text("Crear"),
-                    style: ElevatedButton.styleFrom(
-                      primary: colorBotones,
-                    ),
-                  ),
+                    onPressed: () {
+                      aniadirPaso(paso);
+                    },
+                    child: Text("Añadir pasos"),
+                  )
                 ],
+              ),
+            ),
+
+            // Pasos
+            Visibility(
+              visible: listaPasos.isNotEmpty,
+              child: Container(
+                height: MediaQuery.of(context).size.width / 2,
+                width: MediaQuery.of(context).size.width - separacionElementos,
+                padding: EdgeInsets.fromLTRB(separacionElementos, separacionElementos, separacionElementos, 0.0),
+                child: ListView(
+                  children: listaPasos.map((pasoAux) {
+                    return Card(
+                      child: Container(
+                        padding: EdgeInsets.all(separacionElementos),
+                        child: Text((listaPasos.indexOf(pasoAux) + 1).toString() + ". " + pasoAux),
+                      )
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+
+            // Botón Crear
+            Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width - separacionElementos,
+              padding: EdgeInsets.fromLTRB(separacionElementos, separacionElementos, separacionElementos, 0.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("Crear"),
               ),
             ),
           ],
